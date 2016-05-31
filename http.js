812976e9f8e76ad13d92b65c1425dd176d54f983
cyj7 <cyj19900104@126.com>
2016-05-31 10:23:03 +0800

    修改文件目录

--- http.js
+++ http.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 9090;
 
 app.get("/", function(req, res){
-	res.sendFile(path.join(__dirname, './view/index.html'));
+    res.sendFile(path.join(__dirname, './view/index.html'));
 });
 
 const server = app.listen(port, function(){
-	const host = server.address().address;
+    const host = server.address().address;
     const port = server.address().port;
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require("path");
 var webpack = require("webpack");
 
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
 var node_modules = path.resolve(__dirname, 'node_modules');
 var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 var staticPath = 'assets';
@@ -8,24 +10,21 @@ var port = 4001;
 module.exports = {
   entry: [
     'webpack/hot/dev-server',
-    './static/src/Main.js'
+    './app/Main.js'
   ],
   output: {
     path: path.resolve(__dirname, staticPath),
-    publicPath: "/assets/dev/js/",
-    filename: '/dev/js/build.js'
+    publicPath: "/",
+    filename: 'assets/dev/js/build.js'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', 'sass'],
-    alias: {
-      'react': pathToReact
-    }
+    extensions: ['', '.js', '.jsx', 'sass']
   },
   module: {
     loaders: [
       {
         test: /\.js?$/,
-        exclude: /node_modules/,
+        exclude: node_modules,
         loader: 'babel',
         query: {
           "presets": ["react", "es2015", "stage-0", "react-hmre"]
@@ -42,6 +41,18 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin("css/[name].css"),    //单独使用style标签加载css并设置其路径
+    new HtmlWebpackPlugin({                        //根据模板插入css/js等生成最终HTML
+       favicon:'./src/img/favicon.ico', //favicon路径
+       filename:'/view/index.html',    //生成的html存放路径，相对于 path
+       template:'./app/index.html',    //html模板路径
+       inject: 'body',    //允许插件修改哪些内容，包括head与body
+       hash:true,    //为静态资源生成hash值
+       minify:{    //压缩HTML文件
+           removeComments:true,    //移除HTML中的注释
+           collapseWhitespace:false    //删除空白符与换行符
+       }
+    })
   ]
 };
